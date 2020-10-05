6b3be9320bd28d065194b5e1502748ca300a52e8
peter88tom <peter88tom@gmail.com>
2020-10-05 17:46:28 +0200

    Set proceholder for displaying graph and query results

--- weather_api/views.py
+++ weather_api/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render,redirect
 from django.http import request, HttpResponseRedirect,HttpResponse,JsonResponse
 from django.core import serializers
 from .models import *
+import time
 
 def get_weather(request):
 	if request.method == 'POST':
 		# ToDo
 		# posted data, fetch the weather from any api
+		time.sleep(5)
 		pass
 	else:
 		periods = Period.objects.all()
